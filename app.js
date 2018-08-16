960dcf4fc24ef9a2c8edbc5e1c1f3daabdb01953
DESKTOP-JB2GHIU\RRome <rromero326@msn.com>
2018-08-15 20:16:43 -0600

    JS: String Interpolation II

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
-let favoriteAnimal = 'gorilla';
-console.log('My favorite animal:' + favoriteAnimal + '.');
+let myName = 'Renee';
+let myCity = 'San Francisco';
+
+console.log(`My name is ${myName}. My favorite city is ${myCity}.`);
