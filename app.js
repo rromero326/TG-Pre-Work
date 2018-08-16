9b062c2574faa0a10fc380789493c81eb6cf0c22
DESKTOP-JB2GHIU\RRome <rromero326@msn.com>
2018-08-16 17:27:51 -0600

    JS: Return II

--- app.js
+++ app.js
@@ -13,4 +13,14 @@ const getSubTotal = (itemCount) => {
   return itemCount * 7.5;
 };
 
+const getTax = (itemCount) => {
+  return getSubTotal(itemCount) * 0.06
+}
+
+const getTotal = (itemCount) => {
+  return getTax(itemCount) + getSubTotal(itemCount)
+}
+
 console.log(getSubTotal(orderCount));
+
+console.log(getTotal(orderCount))
