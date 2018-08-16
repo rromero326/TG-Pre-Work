209b68e6b9b0390cfa557c2ba6e24d8e43249730
DESKTOP-JB2GHIU\RRome <rromero326@msn.com>
2018-08-15 22:06:31 -0600

    JS: Ternary Operator

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
-let moonPhase = 'full';
+let isLocked = false;
+isLocked ? console.log('You will need a key to open the door.') : console.log('You will not need a key to open the door.');
 
-switch (moonPhase) {
-  case 'full':
-    console.log('Howl!');
-    break;
-  case 'mostly full':
-  	console.log('Arms and legs are getting hairier');
-    break;
-  case 'mostly new':
-    console.log('Back on two feet');
-    break;
-  default:
-    console.log('Invalid moon phase');
-    break;
-}
+let isCorrect = true;
+isCorrect ? console.log('Correct!') : console.log('Incorrect!');
+
+let favoritePhrase = false;
+favoritePhrase ? console.log('I love that!') : console.log("I don't love that!");
