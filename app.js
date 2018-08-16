c2849fade715ccc3d1ade28556de704f310fc115
DESKTOP-JB2GHIU\RRome <rromero326@msn.com>
2018-08-15 20:59:37 -0600

    JS: Comparison Operators II

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-let hungerLevel = 10;
-if (hungerLevel > 7) {
-  console.log('Time to eat!');
+let moonPhase = 'full';
+
+if (moonPhase === 'full') {
+  console.log('Howl!');
 } else {
-  console.log('We can eat later!');
+  console.log('I swear I am not a werewolf.');
 }
