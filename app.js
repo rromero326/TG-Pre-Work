461a4d29c78bb7ebc6779a32442383737ae6800e
DESKTOP-JB2GHIU\RRome <rromero326@msn.com>
2018-08-16 16:29:27 -0600

    JS: Parameters II

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-const takeOrder = (topping) => {
-  console.log('Order: pizza topped with' + topping);
+const takeOrder = (topping, crustType) => {
+  console.log('Order: pizza topped with ' + topping + crustType);
 };
 
-takeOrder('mushrooms');
+takeOrder('mushrooms', ' thin crust');
+takeOrder('pepperoni', ' deep dish crust');
+takeOrder('garlic', ' rolled crust');
