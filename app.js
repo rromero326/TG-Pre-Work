e07909875e23244b29852a742411ccf2e38647e1
DESKTOP-JB2GHIU\RRome <rromero326@msn.com>
2018-08-15 19:53:36 -0600

    JS: Mathematical Assignment Operators

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-let notDefined;
-let valueless;
+let molecule = 16;
+let particle = 18;
+let assay = 3;
+
+// Add and assign to molecule below
+molecule +=16;
+
+// Multiply and assign to particle below
+particle *= 6.02;
+
+// Increment assay by 1
+assay++;
