513b4cba7a9f817cdc792c2f3fcd3f77dc3f59b7
DESKTOP-JB2GHIU\RRome <rromero326@msn.com>
2018-08-17 13:27:11 -0600

    JS: Constructor

--- app.js
+++ app.js
@@ -1,25 +1,6 @@
-class Dog {
-  constructor(name) {
-    this._name = name;
-    this._behavior = 0;
-  }
-
-  get name() {
-    return this._name;
-  }
-  get behavior() {
-    return this._behavior;
-  }
-
-  incrementBehavior() {
-    this._behavior ++;
+class Surgeon {
+  constructor(name, department) {
+    this.name = name;
+    this.department = department;
   }
 }
-
-
-const halley = new Dog('Halley');
-console.log(halley.name); // Print name value to console
-console.log(halley.behavior); // Print behavior value to console
-halley.incrementBehavior(); // Add one to behavior
-console.log(halley.name); // Print name value to console
-console.log(halley.behavior); // Print behavior value to console
