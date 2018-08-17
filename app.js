8be27fa90cc319ca9138d8bcc7c0b031dfd31a11
DESKTOP-JB2GHIU\RRome <rromero326@msn.com>
2018-08-16 19:49:22 -0600

    JS: Global Scope II

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-const satellite = 'The Moon'
-const galaxy = 'The Milky Way'
-let stars = 'North Star'
+const satellite = 'The Moon';
+const galaxy = 'The Milky Way';
+
+let stars = 'North Star';
 
 const myNightSky = () => {
-	return 'Night Sky: ' + satellite + ', ' + stars + ', ' + galaxy;
+  stars = 'Sirius';
+  return 'Night Sky: ' + satellite + ', ' + stars + ', ' + galaxy;
 };
 
 console.log(myNightSky());
+console.log(stars);
