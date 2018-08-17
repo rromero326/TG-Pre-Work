a38443007f2e9e6c0a8679495a8a552f00f55426
DESKTOP-JB2GHIU\RRome <rromero326@msn.com>
2018-08-17 10:46:54 -0600

    JS: Accessing Object Properties III

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 let person = {
   name: 'Renee',
-  age: 41
+  age: 41,
+  weekendAlarm: 'No alarms needed',
+  weekAlarm: 'Alarm set to 7AM'
 };
+
 console.log(person['name']);
 console.log(person['age']);
+
+let day = 'Friday';
+let alarm;
+
+if (day === 'Saturday' || day === 'Sunday' ) {
+  alarm = 'weekendAlarm';
+} else {
+  alarm = 'weekAlarm';
+}
+
+console.log(person[alarm]);
