9922a6f9daf0ec095b8cb1adcb6a71f602719b08
DESKTOP-JB2GHIU\RRome <rromero326@msn.com>
2018-08-17 08:56:21 -0600

    JS: .forEach()

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
-let cards = ['Diamond', 'Spade', 'Heart', 'Club'];
+let fruits = ['mango', 'papaya', 'pineapple', 'apple'];
 
-let currentCard = 'Heart';
-
-while (currentCard !== 'Spade') {
-  console.log(currentCard);
-  currentCard = cards[Math.floor(Math.random() *4)];
-}
-
-console.log('You found a spade!');
+// Iterate over fruits below
+fruits.forEach(fruitItem => console.log('I want to eat a ' + fruitItem));
+;
