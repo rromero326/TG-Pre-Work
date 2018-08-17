d40848ec021c3f2d2dc35700c62e823b0c5e7e25
DESKTOP-JB2GHIU\RRome <rromero326@msn.com>
2018-08-16 20:07:47 -0600

    JS: Block Scope

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
-const satellite = 'The Moon';
-const galaxy = 'The Milky Way';
-
-let stars = 'North Star';
-
-const myNightSky = () => {
-  stars = 'Sirius';
-  return 'Night Sky: ' + satellite + ', ' + stars + ', ' + galaxy;
-};
-
-console.log(myNightSky());
-console.log(stars);
+const visibleLightWaves = () => {
+  let lightWaves = 'Moonlight';
+  console.log(lightWaves);
+}
+visibleLightWaves();
+console.log(lightWaves);
