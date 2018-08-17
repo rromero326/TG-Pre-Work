238c0a5cc3a1806af360e12adace19eae7f52a1e
DESKTOP-JB2GHIU\RRome <rromero326@msn.com>
2018-08-17 12:36:17 -0600

    JS: Getters and Setters I

--- app.js
+++ app.js
@@ -1,42 +1,15 @@
 let person = {
-  name: 'Renee',
-  age: 41,
-  weekendAlarm: 'No alarms needed',
-  weekAlarm: 'Alarm set to 7AM',
+  _name: 'Lu Xun',
+  _age: 137,
 
-  sayHello: function() {
-    return `Hello, my name is ${this.name}`;
-  },
-
-  sayGoodbye() {
-    return 'Goodbye!';
+  set age(ageIn) {
+    if (typeof ageIn === 'number') {
+      this._age = ageIn;
+    }
+    else {
+      console.log('Invalid input');
+      return 'Invalid input';
+    }
   }
+
 };
-
-let friend = {
-  name: 'Linda'
-};
-
-friend.sayHello = person.sayHello;
-
-console.log(friend.sayHello());
-console.log(person.sayHello());
-
-person.hobbies = ['Cook', 'Bake'];
-person.hobbies = ['Cook'];
-console.log(person.hobbies);
-
-
-console.log(person['name']);
-console.log(person['age']);
-
-let day = 'Tuesday';
-let alarm;
-
-if (day === 'Saturday' || day === 'Sunday' ) {
-  alarm = 'weekendAlarm';
-} else {
-  alarm = 'weekAlarm';
-}
-
-console.log(person[alarm]);
